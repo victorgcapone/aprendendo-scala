d7219e0a4908a4c84b5583923f9323c36c39e1c2
Victor Capone <victor.capone@matera.com>
2018-08-28 15:07:47 -0300

    Adicionando gradle e começando a escrever o Mime em Scala

---
+++ src/main/scala/learning/mime/MimeApplication.scala
@@ -0,0 +1,8 @@
+package learning.mime
+
+
+object MimeApplication {
+  def main(args:Array[String]) = {
+    println("Mime Machine Learning Explanator")
+  }
+}
---
+++ src/main/scala/learning/mime/core/MimeDataWrapper.scala
@@ -0,0 +1,5 @@
+package learning.mime.core
+// Used to encapsulate all data and metadata used by scala.learning.mime for message passing beetwen layers
+case class MimeDataWrapper(){
+
+}
