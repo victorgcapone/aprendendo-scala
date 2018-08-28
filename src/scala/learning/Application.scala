8647d6651965ba3f97217579a67c2196e4ca8ff8
Victor Capone <victor.capone@matera.com>
2018-08-28 10:36:48 -0300

    Mudando filtro para separa numeros pares

--- src/scala/learning/Application.scala
+++ src/scala/learning/Application.scala
@@ -3,10 +3,12 @@ package scala.learning
 object Application {
   def main(args: Array[String]){
     println("Hello World!");
-    var it = new IntegerIterator(Array[Integer](1,2,3));
+    var it = new IntegerIterator(Array[Integer](1,2,3,4,6));
+
     var filter = new Filter[Integer]((e: Integer) => {
-      e < 3
+      e % 2 == 0
     })
+
     var filtered = new IntegerIterator(filter.filter(it));
     while (filtered hasNext){
       println(filtered next);
