f3520b4f86448ef76a3aa7d60cb480f03e945005
Victor Capone <victor.capone@matera.com>
2018-08-30 14:20:38 -0300

    Atualizando exemplo spark

--- src/main/scala/spark/SparkApplication.scala
+++ src/main/scala/spark/SparkApplication.scala
@@ -2,8 +2,12 @@ package spark
 
 
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.ml.feature.LabeledPoint
+import org.apache.spark.mllib.tree.DecisionTree
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.array
+import org.apache.spark.sql.functions._
+import org.apache.spark.ml.linalg.Vector
 
 object SparkApplication {
   Logger.getLogger("org").setLevel(Level.ERROR)
@@ -26,8 +30,14 @@ object SparkApplication {
     data.printSchema()
     //Agrupa as features em uma única coluna vetor
     val groupedData = data.select(array($"petal_length", $"petal_width",
-                                            $"sepal_length", $"sepal_width") as "features",
+                                        $"sepal_length", $"sepal_width") as "features",
                                         $"species" as "label")
+                          //Transforma as labels em números
+                          .withColumn("label", when($"label".equalTo("versicolor"), 1.0)
+                            .otherwise(when($"label".equalTo("virginica"), 2.0)
+                            .otherwise(3.0)))
+                          // Transforma em um dataset de pontos com labels
+                          .map(r => {new LabeledPoint(r.getAs[Double]("label"), r.getAs[Vector]("features"))})
     groupedData.printSchema()
     groupedData.show(10)
     // Amostra de 70% sem reposição para o treino
